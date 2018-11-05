fd16617f74e29ac85971bf29fb7fdae7dd1ef90d
leejianzhi <lijianzhimomo@gmail.com>
2018-11-05 18:47:47 -0500

    login finished
    To Do
    register function

---
+++ Assignment3/src/java/Assignment3_db/Login_db.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment3_db;
+import Assignment3_beans.Login_bean;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Jianzhi
+ */
+public class Login_db {
+    
+    public String authorizeLogin(Login_bean login_bean) //create authorizeLogin()function
+    {   
+        //store username from login_bean
+        String username=login_bean.getUsername(); 
+        //store password from login_bean
+        String password=login_bean.getPassword(); 
+        
+        //creat variable for username to access the database
+        String dbusername="";  
+        String dbpassword="";
+        //connect to the database
+        String url="jdbc:derby://localhost:1527/Assignment3 [IS2560 on IS2560]"; 
+        String uname="IS2560"; //database username
+        String pass=""; //database password
+        
+        try
+        {
+               //load driver
+            Class.forName("com.mysql.jdbc.Driver"); 
+            //create connection
+            Connection con=DriverManager.getConnection(url,uname,pass); 
+            //sent SQL query to the database
+            PreparedStatement ps=null; //create statement
+            //SQL for select the username and password
+            ps=con.prepareStatement("select * from user where username=? and password=?"); 
+            ps.setString(1,username);
+            ps.setString(2,password);
+            //excute query
+            ResultSet rs=ps.executeQuery(); 
+             
+            while(rs.next())
+            {    
+                //fetch username and password from the variable we created above which is dbusername nad dbpassword
+                dbusername=rs.getString("username");  
+                dbpassword=rs.getString("password"); 
+                
+                //see if the user input matched both the username and password
+                if(username.equals(dbusername) && password.equals(dbpassword))  
+                {
+                    // if the username and password matched the data in database then the user is able to successfully login
+                    return "You are successfully login"; 
+                }
+            } 
+           //close prepared statement
+            ps.close(); 
+            //close connection
+            con.close(); 
+           
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        //if either or both the input of username or password is incorrect the meesage below will show up to the user
+        return "Your username or password is incorrect, please try again.";
+    }
+    
+}
--- Assignment3/src/java/Assignment3_servlet/Login_servlet.java
+++ Assignment3/src/java/Assignment3_servlet/Login_servlet.java
@@ -5,17 +5,26 @@
  */
 package Assignment3_servlet;
 
+import Assignment3_beans.Login_bean;
+import Assignment3_db.Login_db;
+
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
- *
  * @author Jianzhi
- */
+ *
+ *dSource code modified from http://met.guc.edu.eg/OnlineTutorials/JSP%20-%20Servlets/Full%20Login%20Example.aspx
+ * 
+ **/
+ 
+
 public class Login_servlet extends HttpServlet {
 
     /**
@@ -30,57 +39,44 @@ public class Login_servlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Login_servlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Login_servlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        
+        //check if the login button has been tra
+        if(request.getParameter("btn_login")!=null) 
+        {
+            String username=request.getParameter("username_imput"); //get textbox name "txt_username"
+            String password=request.getParameter("password_imput"); //get textbox name "txt_password"
+            
+            Login_bean login_bean =new Login_bean(); //this class contain seeting up all received values from index.jsp page to setter and getter method for application require effectively 
+            
+            //username set from the login_bean
+            login_bean.setUsername(username); 
+            //password set from the login_bean
+            login_bean.setPassword(password); 
+            //access the login database component and vadicate the login info from the database
+            Login_db login_db=new Login_db(); 
+            String authorize=login_db.authorizeLogin(login_bean);
+            
+            //if the username and password from the user input is passed the authorization then will shows the user success login info, 
+            //otherwise to let user check the username or password and try to login again
+            if(authorize.equals("Welcome, you are successfully login")) 
+            {
+                HttpSession session=request.getSession(); //session is created
+                //The username will store in getUsername in login_bean 
+                session.setAttribute("login",login_bean.getUsername()); 
+                //direct to the homepage if the user successfully login
+                RequestDispatcher rd=request.getRequestDispatcher("home.jsp"); 
+                rd.forward(request, response);
+            }
+            else
+            {
+                request.setAttribute("WrongLoginMsg",authorize); 
+                //go back to index page which is login page
+                RequestDispatcher rd=request.getRequestDispatcher("index.jsp"); 
+                rd.include(request, response);
+            }
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
-}
